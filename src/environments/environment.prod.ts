d45b219cca475cd687eca4cbbfb00c6238edce3b
Johan Viancha <vianchajohan@gmail.com>
2022-10-08 11:39:57 -0500

    Integrar con firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'red-code-101',
+    appId: '1:318221536042:web:e8ea7c0d8f2c8608c4ac53',
+    storageBucket: 'red-code-101.appspot.com',
+    apiKey: 'AIzaSyA_fquVbD0Kwyg_a07rILMHbKpVpJ7aVwU',
+    authDomain: 'red-code-101.firebaseapp.com',
+    messagingSenderId: '318221536042',
+    measurementId: 'G-YF8RJZX7XQ',
+  },
   production: true
 };
