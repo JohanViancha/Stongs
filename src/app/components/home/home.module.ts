8f3ecf3bc7ad7a6c7431eeb77bb413824a702a97
Johan Viancha <vianchajohan@gmail.com>
2022-10-10 23:59:35 -0500

    initial layout of the nav and the header

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { NavComponent } from 'src/app/shared/organisms/nav/nav.component';
+import { HeaderComponent } from 'src/app/shared/organisms/header/header.component';
+import { FooterComponent } from 'src/app/shared/molecules/footer/footer.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent,
+    NavComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
