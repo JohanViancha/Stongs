acb415b3abe1febe26fd1a433e28186ee569ddee
Andres Rangel <andres.rangel555@gmail.com>
2022-11-10 11:23:10 -0500

    creación del componente table

--- src/app/components/inventory/inventory.module.ts
+++ src/app/components/inventory/inventory.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { InventoryRoutingModule } from './home-routing.module';
+import { InventoryRoutingModule } from './inventory-routing.module';
+import { TableComponent } from 'src/app/shared/molecules/table/table.component';
+import { InventoryComponent } from './inventory.component';
 
 
 @NgModule({
   declarations: [
+    InventoryComponent,
+    TableComponent
   ],
   imports: [
     CommonModule,
