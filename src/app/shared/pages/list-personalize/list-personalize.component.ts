5de27596896abfb7508f9ced6cb248cd0a207e5e
Andres Rangel <andres.rangel555@gmail.com>
2022-11-26 12:00:39 -0500

    creación de page para listado de productos personalizados

---
+++ src/app/shared/pages/list-personalize/list-personalize.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-personalize',
+  templateUrl: './list-personalize.component.html',
+  styleUrls: ['./list-personalize.component.scss']
+})
+export class ListPersonalizeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ErrorFormReactivePipe } from 'src/app/shared/util/pipes/error-form-reactive.pipe';
 import { FormRegisterPersonalizeComponent } from './organisms/form-register-personalize/form-register-personalize.component';
+import { ListPersonalizeComponent } from './pages/list-personalize/list-personalize.component';
 
 
 @NgModule({
   declarations: [
-    ErrorFormReactivePipe
+    ErrorFormReactivePipe,
+    ListPersonalizeComponent
   ],
   exports: [
     ErrorFormReactivePipe
